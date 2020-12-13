6f069007520817bb921a841b7a4d87b84e4a2531
Rafael Miranda <rafaelpadovezi@gmail.com>
2020-12-13 17:28:15 -0300

    build: change compose to use elk image

--- scripts/start-from-compose.sh
+++ scripts/start-from-compose.sh
@@ -6,4 +6,4 @@ echo "[PRE-RUN]: Building project..."
 dotnet build $CSPROJ_PATH
 
 echo "[PRE-RUN]: Watching and running project..."
-dotnet watch --project $CSPROJ_PATH run --urls http://$ASPNETCORE_SOCKET_BIND_ADDRESS:$ASPNETCORE_SOCKET_BIND_PORT
+dotnet watch --project $CSPROJ_PATH run --urls $ASPNETCORE_URLS
